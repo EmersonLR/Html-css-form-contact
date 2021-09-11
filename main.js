86fee6b20aac843365e0ff02e9e62115cbf44972
Doug-oss <douglasj.sjc@gmail.com>
2021-09-11 14:21:30 -0300

    Adicionado botao fechar

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-console.log("Hello Word")
-
-
 var email = document.getElementById("mail");
-var nome = document.getElementById("nome");
-var tel = document.getElementById("fone");
-var txt = document.getElementById("text");
+var box = document.querySelector(".contact-box")
+var btnClose = document.querySelector(".uk-close")
+var boxImg = document.querySelectorAll(".container")
 
 
 email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
+  if (email.validity.typeMismatch || email.v) {
     email.setCustomValidity("Preencha o email, corretamente!");
   } else {
     email.setCustomValidity("");
   }
 });
+
+btnClose.addEventListener("click", function (event) {
+  box.style.display = "none";
+  box.style.filter = "blur(0)";
+})
+
+/*------ Tirar X btn ----*/
+var x = window.matchMedia("(max-width: 880px)")
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    btnClose.style.display = "none";
+  } else {
+    btnClose.style.display = "block";
+  }
+}
+
+myFunction(x)
+
+x.addListener(myFunction)
+
